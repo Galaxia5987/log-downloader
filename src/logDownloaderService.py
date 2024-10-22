4ae66f2af152ea10e35f00bb62da5fe8dff61787
rakrakon <68773850+rakrakon@users.noreply.github.com>
2024-10-22 20:41:39 +0300

    Create monitor_devices function

--- src/logDownloaderService.py
+++ src/logDownloaderService.py
@@ -1,4 +1,5 @@
 import hashlib
+from loguru import logger
 
 def hash_file(filepath):
     hasher = hashlib.md5()
@@ -6,3 +7,16 @@ def hash_file(filepath):
         buf = f.read()
         hasher.update(buf)
     return hasher.hexdigest()
+
+def monitor_devices():
+    while True:
+        devices = [disk.device for disk in psutil.disk_partitions()]
+        for device in devices:
+            if "roboRIO" in device or "/media" in device:
+                logger.info(f"Device connected: {device}")
+
+        time.sleep(10)
+
+
+if __name__ == "__main__":
+    monitor_devices()

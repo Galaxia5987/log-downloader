c51eb1e203865b38cd7b59b2089f885594606489
rakrakon <68773850+rakrakon@users.noreply.github.com>
2024-10-22 20:40:46 +0300

    Create hash_file function

---
+++ src/logDownloaderService.py
@@ -0,0 +1,8 @@
+import hashlib
+
+def hash_file(filepath):
+    hasher = hashlib.md5()
+    with open(filepath, 'rb') as f:
+        buf = f.read()
+        hasher.update(buf)
+    return hasher.hexdigest()
